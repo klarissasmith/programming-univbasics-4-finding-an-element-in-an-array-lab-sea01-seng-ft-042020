a7dc84c5dce8232f4f7a2ce37175ca51d7d63623
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-03 21:48:49 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0;
   while counter < array.length do
-    new_array.push(array[counter])
+    if array[counter] == value_to_find
+      return counter
+    end
     counter += 1
   end
+  
 end
